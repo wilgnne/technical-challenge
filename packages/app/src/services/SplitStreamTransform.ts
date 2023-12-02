959f114b05a6016e657dd33f610296096ac17a62
Wilgnne Khawan <wilgnne.kba@gmail.com>
2023-12-02 22:27:40 +0000

    refector: SplitStreamTransform

--- packages/app/src/services/SplitStreamTransform.ts
+++ packages/app/src/services/SplitStreamTransform.ts
@@ -4,19 +4,19 @@ export class SplitStreamTransform extends Transform {
   private internalBuffer: string | undefined = "";
 
   constructor(private readonly separator: string | RegExp) {
-    super({
-      transform: (chunk: string | Buffer, _: BufferEncoding, callback: TransformCallback): void => {
-        const chunkBuffer = chunk.toString();
-        const buffer = this.internalBuffer + chunkBuffer;
+    super()
+  }
 
-        const items = buffer.split(this.separator);
-        items.slice(0, -1).forEach((item) => this.push(item));
+  _transform(chunk: string | Buffer, _: BufferEncoding, callback: TransformCallback) {
+    const chunkBuffer = chunk.toString();
+    const buffer = this.internalBuffer + chunkBuffer;
 
-        this.internalBuffer = items[items.length - 1];
+    const items = buffer.split(this.separator);
+    items.slice(0, -1).forEach((item) => this.push(item));
 
-        callback(null);
-      }
-    })
+    this.internalBuffer = items[items.length - 1];
+
+    callback(null);
   }
 
   _flush(callback: TransformCallback): void {

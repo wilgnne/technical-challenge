f200448404b255e842be9327038bd79806a7eb52
Wilgnne Khawan <wilgnne.kba@gmail.com>
2023-12-02 20:13:28 +0000

    feat: SplitStreamTransform implementation & tests

---
+++ packages/app/src/services/SplitStreamTransform.ts
@@ -0,0 +1,25 @@
+import { Transform, TransformCallback } from "node:stream";
+
+export class SplitStreamTransform extends Transform {
+  private internalBuffer: string | undefined = "";
+
+  constructor(private readonly separator: string | RegExp) {
+    super({
+      transform: (chunk: string | Buffer, _: BufferEncoding, callback: TransformCallback): void => {
+        const chunkBuffer = chunk.toString();
+        const buffer = this.internalBuffer + chunkBuffer;
+
+        const items = buffer.split(this.separator);
+        items.slice(0, -1).forEach((item) => this.push(item));
+
+        this.internalBuffer = items[items.length - 1];
+
+        callback(null);
+      }
+    })
+  }
+
+  _flush(callback: TransformCallback): void {
+    callback(null, this.internalBuffer);
+  }
+}

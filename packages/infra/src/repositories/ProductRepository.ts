4f753d044e58b3785aef226b0e86766239e0773a
Wilgnne Khawan <wilgnne.kba@gmail.com>
2023-12-04 00:13:31 +0000

    feat: product exist by id repository

--- packages/infra/src/repositories/ProductRepository.ts
+++ packages/infra/src/repositories/ProductRepository.ts
@@ -5,6 +5,15 @@ import { ClientBase } from "pg";
 class ProductRepository {
   constructor(private readonly client: ClientBase) {}
 
+  async existById(productId: number, orderId: number): Promise<boolean> {
+    const result = await this.client.query<{ exists: true }>(
+      "SELECT EXISTS (SELECT 1 FROM public.product WHERE public.product.product_id = $1 AND public.product.order_id = $2)",
+      [productId, orderId],
+    );
+
+    return result.rows[0]?.exists ?? false;
+  }
+
   async insert(produc: Product): Promise<void> {
     await this.client.query(
       "INSERT INTO public.product (product_id, order_id, value) VALUES ($1, $2, $3)",

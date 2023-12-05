c5435d6e3584e4852bb566b44ac34657f32c6646
Wilgnne Khawan <wilgnne.kba@gmail.com>
2023-12-05 04:12:53 +0000

    feat: order service

--- packages/app/src/dtos/index.ts
+++ packages/app/src/dtos/index.ts
@@ -1,1 +1,2 @@
 export * from "./DataRowDto";
+export * from "./OrderQueryDto";
--- packages/app/src/services/index.ts
+++ packages/app/src/services/index.ts
@@ -1,7 +1,13 @@
 import SplitStreamTransform from "./SplitStreamTransform";
 import DataRowInsertWritable from "./DataRowInsertWritable";
 import StreamFileUploadService from "./StreamFileUploadService";
+import OrderService from "./OrderService";
 
 export * from "./types";
 
-export { SplitStreamTransform, DataRowInsertWritable, StreamFileUploadService };
+export {
+  SplitStreamTransform,
+  DataRowInsertWritable,
+  StreamFileUploadService,
+  OrderService,
+};
